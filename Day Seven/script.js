b08d31564fb5fece0ac8585010832a30fa3a2e74
Nishi-Kun <erikdasilvaferreira@hotmail.com>
2025-02-27 21:35:49 -0300

    dia 7

--- Day Seven/script.js
+++ Day Seven/script.js
@@ -7,40 +7,48 @@ let operacao = solicitarOperacao();
 let primeiroNumero;
 let segundoNumero;
 
+while(operacao.toLowerCase() != '5'){
 
-switch(operacao.toLowerCase()){
-    case '1':
-        primeiroNumero = solicitarNumero();
-        segundoNumero = solicitarNumero();
-        alert('somando');
-        alert(somar(primeiroNumero,segundoNumero));
-        break;
+    switch(operacao.toLowerCase()){
+        case '1':
+            primeiroNumero = solicitarNumero();
+            segundoNumero = solicitarNumero();
+            alert('somando');
+            alert(somar(primeiroNumero,segundoNumero));
+            break;
 
-    case '2':
-        primeiroNumero = solicitarNumero();
-        segundoNumero = solicitarNumero();
-        alert('subtraindo');
-        alert(subtrair(primeiroNumero,segundoNumero));
-        break;
+        case '2':
+            primeiroNumero = solicitarNumero();
+            segundoNumero = solicitarNumero();
+            alert('subtraindo');
+            alert(subtrair(primeiroNumero,segundoNumero));
+            break;
 
-    case '3':
-        primeiroNumero = solicitarNumero();
-        segundoNumero = solicitarNumero();
-        alert('multiplicando');
-        alert(multiplicar(primeiroNumero,segundoNumero));
-        break;
+        case '3':
+            primeiroNumero = solicitarNumero();
+            segundoNumero = solicitarNumero();
+            alert('multiplicando');
+            alert(multiplicar(primeiroNumero,segundoNumero));
+            break;
 
-    case '4':
-        primeiroNumero = solicitarNumero();
-        segundoNumero = solicitarNumero();
-        alert('dividindo');
-        alert(dividir(primeiroNumero,segundoNumero));
-        break;
+        case '4':
+            primeiroNumero = solicitarNumero();
+            segundoNumero = solicitarNumero();
+            alert('dividindo');
+            alert(dividir(primeiroNumero,segundoNumero));
+            break;
 
-    case '5':
-        alert('Até a próxima!')
-        break;
+        case '5':
+            alert('Até a próxima!');
+            break;
+    }
+
+    operacao = solicitarOperacao();
 }
+
+alert('Até a próxima!')
+
+
 
 function solicitarNumero(){
     let numeroUsuario = prompt('Digite um número para calcular:');
